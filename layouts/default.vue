1c626d79b613eec9e57eacdb22ece8a1f0879bb0
rahultwins1995 <user9.ttpl@gmail.com>
2025-04-03 18:16:22 +0530

    updated layout file

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,18 @@
 <script>
 import Header from '~/components/Header.vue';
 import Footer from '~/components/Footer.vue';
+import VehicleSelector  from '~/components/VehiclePop.vue';
 export default {
     components: {
         Header,
+        VehicleSelector,
         Footer
     }
 }
 </script>
 <template>
     <Header></Header>
+    <VehicleSelector></VehicleSelector>
     <NuxtPage />
     <Footer></Footer>
 </template>
